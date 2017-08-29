dc53ab4e6ecfb6077f0a9228bdad3bb225e1e9a3
JSP <jspenkar@gmail.com>
2017-08-28 20:39:52 -0500

    Header component and base style sheet added.

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import * as gallup from '../gallup'
+import React from 'react';
+import * as gallup from '../gallup';
+import {Header} from '../components'
+
+require('../style/app.scss');
 
 const App = () => {
   return (
     <div id='app'>
+      { Header() }
     </div>
   )
 };
