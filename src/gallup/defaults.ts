3302f191bf530780f3bd207b3faed7d33a5d53e8
JSP <jspenkar@gmail.com>
2023-12-22 00:35:25 -0600

    Convert gallup files to TS

---
+++ src/gallup/defaults.ts
@@ -0,0 +1,67 @@
+const defaults = {
+  subtitle: {
+    text: `Source: Gallup.com`,
+  },
+  xAxis: {
+    title: {
+      text: `Date`,
+    },
+    type: `datetime`,
+  },
+  legend: {
+    layout: `vertical`,
+    align: `right`,
+    verticalAlign: `bottom`,
+    x: 0,
+    y: 0,
+  },
+  yAxis: {
+    title: {
+      text: `Approval Percentage`,
+    }
+  },
+  tooltip: {
+    pointFormat: "<span style='color:{series.color}'>{series.name}</span>: <b>{point.y}%</b><br/>",
+    split: true
+  },
+  plotOptions: {
+    area: {
+      stacking: "percent",
+      lineColor: "#ffffff",
+      lineWidth: 1,
+      marker: {
+        lineWidth: 1,
+        lineColor: "#ffffff"
+      }
+    }
+  },
+}
+
+export const overlapGraph = Object.assign({}, defaults, {
+  chart: {
+    height:window.innerHeight - 64,
+    type:"line",
+  },
+  title: {
+    text: `Presidential approval and disapproval for the prior four presidents.`,
+  },
+  legend: {
+    layout:`horizontal`,
+    align:`center`,
+    verticalAlign:`bottom`,
+  }
+});
+
+export const approvalChartDefault = Object.assign({}, defaults, {
+  chart: {
+    height:window.innerHeight - 64,
+    type:`area`,
+  },
+});
+
+export const approvalByParty = Object.assign({}, defaults, {
+  chart: {
+    height:window.innerHeight - 64,
+    type:`line`,
+  }
+});
